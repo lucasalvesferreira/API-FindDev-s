ee0efdbfd8c6b83630772387d90b1f1503355290
unknown <lucas.aferreira@bandtec.com.br>
2023-03-12 03:49:18 -0300

    feat - adcionando .jar no projeto

--- src/test/java/projetopi/finddevservice/FindDevServiceApplicationTests.java
+++ src/test/java/projetopi/finddevservice/FindDevServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package projetopi.finddevservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FindDevServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package projetopi.finddevservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FindDevServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
