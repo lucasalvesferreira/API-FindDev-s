739a3a8a2ecafe26fc9817e12af328262945518b
unknown <lucas.aferreira@bandtec.com.br>
2023-05-28 15:13:33 -0300

    fix- ajuste

--- src/main/java/projetopi/finddevservice/config/SecurityConfig.java
+++ src/main/java/projetopi/finddevservice/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST", "PUT", "DELETE");
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 
     @Bean
